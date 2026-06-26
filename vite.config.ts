f58fd50431c99c30884435ebb2ef1bac0a33d94d
abeliwi1 <abeliwi1@jh.edu>
2026-06-25 21:45:11 -0400

    fix: ensure correct app deployment base in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/FluxTrak-issue-tracker-app/', // app deployment base
   plugins: [react()],
   resolve: {
     alias: {
